d041e63dcf5961dbde9b4549a19f036c5af95847
TheAstroProject <theastrocodingproject@gmail.com>
2024-02-13 21:29:52 -0700

    added player choice and its return

--- script.js
+++ script.js
@@ -37,3 +37,27 @@ function getPlayerSelection() {
     }
     return playerChoice
 }
+
+// we take player choice and computer choice and compare it. then return the winner
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+
+    if (playerSelection === "paper" && computerSelection == "rock") {
+        result = "You Win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection == "scissors") {
+        result = "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "rock" && computerSelection == "scissors") {
+        result = "You Win! Rock beats Scissors";
+    } else if (playerSelection === "rock" && computerSelection == "paper") {
+        result = "You Lose! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection == "paper") {
+        result = "You Win! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection == "rock") {
+        result = "You Lose! Rock beats Scissors";
+    } else {
+        result = `Draw! ${playerSelection} equals ${computerSelection}`;
+    }
+}
+
+//play 5 times in a row and check who win bo5
