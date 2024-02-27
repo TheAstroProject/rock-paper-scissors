2e08cbb44f54bf6ea5b1073d797e07a47eaf81be
Astro Aguilar <theastrocodingproject@gmail.com>
2024-02-26 22:49:24 -0700

    rewrote code to fix some issues, final issue is getting my buttons to work

--- script.js
+++ script.js
@@ -1,98 +1,72 @@
-//rewriting code in a different manner as I could not find a previous solution
+// rewriting code from scratch to test out neatness
 
-//Defining the variables that we are going to use 
-let result = null
-let playerCount = 0
-let computerCount = 0
-let playerWin = 'Player wins!'
-let tie = 'You tied!'
-let computerWin = 'Computer wins!'
-
-// function that obtains the selection of the computer, and shows who won the round
-const game = () => {
-
-    //computer picks a number from 1-3 (values = rock, paper, scissors)
-   const getComputerChoice = () => {
-    let random = Math.floor(Math.random() * 3) + 1;
-    let computerPick = null
-
-    if (random === 1) {
-        computerPick = 'rock'
-    } else if (random === 2 ) {
-        computerPick = 'paper'
+let playerScore = 0;
+let computerScore = 0;
+//creates our random gen
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//gets the computers random input from 1-3 and turns into output of rock, paper, scissors
+function getComputerChoice() {
+    let randomInt = getRndInteger(1, 3);
+    if (randomInt == 1) {
+        return "rock";
+    } else if (randomInt == 2) {
+        return "paper"
     } else {
-        computerPick = 'scissors'
+        return "scissors"
     }
-   
-    return computerPick
 }
-
-// this function gets the players choice of RPS via a Prompt
-const getPlayerChoice = () => {
-    let playerPick = prompt('Choice: Rock - Paper - Scissors ').toLowerCase()
-
-    if (playerPick != 'rock' && playerPick != 'paper' && playerPick != 'scissors') {
-        alert('Enter a valid choice: rock, paper, scissors')
-        return getPlayerChoice()
-    }
-
-    return playerPick
+//this function lets us play a round of rpc
+function playRound (playerSelection, computerScore) {
+    score.style.color = "black";
+      if (playerSelection === computerSelection) {
+        resultText.style.color = "black";
+        resultText.textContent = `You tied this round! Computer chose ${computerSelection}.`;
+            showScore();
+          return 0;
+      } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "scissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock" ) { 
+            playerScore += 1;
+            resultText.style.color = "limegreen";
+            resultText.textContent = `You win this round! Computer chose ${computerSelection}.`;
+            showScore();
+          return 1;
+      } else {
+        computerScore += 1;
+        resultText.style.color = "red";
+        resultText.textContent = `You lose this round! Computer chose ${computerSelection}.`;
+      showScore();
+          return -1;
+      }
+  }
+//lets show the score at the end
+const score = document.querySelector('#score');
+function showScore() {
+    score.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 }
+//creating UI for the game on html, adds live buttons with actions 
+let playerSelection;
+let computerSelection;
+const resultText = document.querySelector('#result');
+const buttons = document.querySelectorAll('.choice');
 
-let computer = getComputerChoice()
-let player = getPlayerChoice()
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.value, getComputerChoice());
+        if (playerScore >= 5 || computerScore >= 5) {
+            gameFinish();
+        };
+    })
+});
 
-// gets computers choice, then players choice, and returns result
-const playRound = (player, computer) => {
-
-    console.log(`The computer chooses: ${computer}`)
-    console.log(`You chose: ${player}`)
-
-    if (player === computer) {
-        result = tie
-        alert(tie)
-        console.log(tie)
-
-    } else if ( player === 'rock' && computer === 'paper' || 
-    player === 'paper' && computer === 'scissors' || 
-    player === 'scissors' && computer === 'rock'){
-        result = computerWin
-        alert(computerWin)
-        console.log(computerWin)
-        
+function gameFinish() {
+    if (playerScore > computerScore) {
+        score.style.color = "limegreen";
+        resultText.textContent = "You Win!!";
     } else {
-
-        result = playerWin
-        alert(playerWin)
-        console.log(playerWin)
-        
-        return result
+        score.style.color = "red";
+        resultText.textContent = "You Lose!!"
     }
-}
-
-    playRound(player, computer)
-}
-
-
-
-// loop for a 5 round game (not a Bo5)
-for (let i = 0; i <= 4; i++) {
-    game()
-
-    if (result == playerWin) {
-        playerCount += 1
-    } else if (result == computerWin) {
-        computerCount += 1
-    }
-    
-    console.log(` Player: ${playerCount} \n Computer: ${computerCount}`)
-}
-
-// alerts you if you win, lose or tie
-if (playerCount > computerCount) {
-    alert('Congratulations, you won!')
-} else if (computerCount > playerCount){
-    alert('Sorry, computer won!')
-} else {
-    alert('Oh a tie!')
+    playerScore = 0;
+    computerScore = 0;
 }
